a49ec0100d53963868275551a5b2408f58ed4691
zv3z <zv3zv9@gmail.com>
2026-05-19 00:43:49 +0300

    Integrate next-intl plugin with Next.js config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
+const withNextIntl = require('next-intl/plugin')();
+
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig = {
-  output: 'export',
-  basePath,
-  assetPrefix: basePath,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+    output: 'export',
+    basePath,
+    assetPrefix: basePath,
+    trailingSlash: true,
+    images: {
+          unoptimized: true,
+    },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
